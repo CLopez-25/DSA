30bb5ce62ddf4c26653d2ba513098992c1a2cb9c
Cesar Lopez <lopez.m.cesar17@gmail.com>
2023-01-31 11:31:46 -0600

    lecture on BigO

--- src/main/java/datastructures/Main.java
+++ src/main/java/datastructures/Main.java
@@ -1,7 +1,23 @@
 package datastructures;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    public static void printItems(int n) {
+        for (int i = 0; i < n; i++) {
+            System.out.println(i);
+        }
+
+        for (int j = 0; j < n; j++) {
+            System.out.println(j);
+        }
     }
+
+    public static void main(String[] args) {
+        printItems(10);
+    }
+
 }
+
+// Big O: is time complexity vs space complexity of an algorithm or data structure
+// code 1 can be faster than code 2 in terms of time complexity but code 2 can be more efficient in terms of space complexity
+//
