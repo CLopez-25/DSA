2aabc2a746e82350e329c06525d15c8ac363df90
Cesar Lopez <lopez.m.cesar17@gmail.com>
2023-01-31 12:20:29 -0600

    Big0 continued

--- src/main/java/datastructures/Main.java
+++ src/main/java/datastructures/Main.java
@@ -2,13 +2,46 @@ package datastructures;
 
 public class Main {
 
-    public static void printItems(int n) {
-        for (int i = 0; i < n; i++) {
-            System.out.println(i);
-        }
+    // the constant is being dropped therefor this is example of 0(n):
+//    public static void printItems(int n) {
+//        for (int i = 0; i < n; i++) {
+//            System.out.println(i);
+//        }
+//
+//        for (int j = 0; j < n; j++) {
+//            System.out.println(j);
+//        }
+//    }
 
-        for (int j = 0; j < n; j++) {
-            System.out.println(j);
+    //  the following is an example of 0(n^2): purpose of quotes is so that it returns a string instead of adding the integers (i/j) together
+//    public static void printItems(int n){
+//        for (int i = 0; i < n; i++) {
+//            for (int j = 0; j < n; j++) {
+//                System.out.println(i + " " + j);
+//            }
+//        }
+//    }
+
+    // the following is an example of 0(n^3) three levels deep now
+//    public static void printItems(int n){
+//        for (int i = 0; i < n; i++) {
+//            for (int j = 0; j < n; j++) {
+//                for (int k = 0; k < n; k++) {
+//                    System.out.println(i + " " + j + " " + k);
+//                }
+//            }
+//        }
+//    }
+
+    // if you can simplify your code to be 0(n^2) it will be much faster than 0(n^3)
+    public static void printItems(int n){
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.println(i + " " + j);
+            }
+        }
+        for (int k = 0; k < n; k++) {
+            System.out.println(k);
         }
     }
 
@@ -20,4 +53,4 @@ public class Main {
 
 // Big O: is time complexity vs space complexity of an algorithm or data structure
 // code 1 can be faster than code 2 in terms of time complexity but code 2 can be more efficient in terms of space complexity
-//
+// if you can simplify your code to be 0(n^2) it will be much faster than 0(n^3)
