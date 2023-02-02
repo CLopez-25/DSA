e145e66fca72c683f3d486532ea598e73967334f
Cesar Lopez <lopez.m.cesar17@gmail.com>
2023-02-02 15:32:06 -0600

    more notes on big0

--- src/main/java/datastructures/Main.java
+++ src/main/java/datastructures/Main.java
@@ -2,7 +2,8 @@ package datastructures;
 
 public class Main {
 
-    // the constant is being dropped therefor this is example of 0(n):
+    //    n + n = 2n or 0(2n)
+    // the constant(2) is being dropped therefor this is example of 0(n):
 //    public static void printItems(int n) {
 //        for (int i = 0; i < n; i++) {
 //            System.out.println(i);
@@ -13,7 +14,7 @@ public class Main {
 //        }
 //    }
 
-    //  the following is an example of 0(n^2): purpose of quotes is so that it returns a string instead of adding the integers (i/j) together
+    //  the following is an example of 0(n^2): purpose of quotes is so that it returns a string instead of adding the integers (int i/ int j) together
 //    public static void printItems(int n){
 //        for (int i = 0; i < n; i++) {
 //            for (int j = 0; j < n; j++) {
@@ -51,6 +52,13 @@ public class Main {
 
 }
 
-// Big O: is time complexity vs space complexity of an algorithm or data structure
+// Big 0: is time complexity vs space complexity of an algorithm or data structure that produces the same output for a given input.
 // code 1 can be faster than code 2 in terms of time complexity but code 2 can be more efficient in terms of space complexity
 // if you can simplify your code to be 0(n^2) it will be much faster than 0(n^3)
+
+// 0(1) is the most efficient Big 0: from what we've seen so far, as n grows, the number of operations grows. in this example, the number of operations stays constant.
+
+// 0(log n) is the second most efficient Big 0.
+
+// some sorting algorithms use 0(n log n) with quick sort and merge sort.
+
