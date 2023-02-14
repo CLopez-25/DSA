ce9527223d9ea2e01ad037a8df01d6eacc4d241b
Cesar Lopez <lopez.m.cesar17@gmail.com>
2023-02-14 15:51:22 -0600

    added comments to rest of append execise solution

--- src/main/java/datastructures/linkedlist/Main.java
+++ src/main/java/datastructures/linkedlist/Main.java
@@ -6,14 +6,16 @@ public class Main {
     public static void main(String[] args) {
 
 //        this is creating a new linked list. variable mylinkedist is of type linked list which means it can point to a linked list.
-        LinkedList myLinkedList = new LinkedList(1);
-        myLinkedList.makeEmpty();
-        myLinkedList.append(1);
-        myLinkedList.append(2);
 
-        myLinkedList.getHead();
-        myLinkedList.getTail();
-        myLinkedList.getLength();
+        LinkedList myLinkedList = new LinkedList(4);  // creating our new linked list with a node with a value of 4 (value was from prev course section).
+
+        myLinkedList.makeEmpty();           // empties the list
+        myLinkedList.append(1);             // appends the new node value with a value of 1
+        myLinkedList.append(2);             // appends the new node value with a value of 2
+
+//        myLinkedList.getHead();
+//        myLinkedList.getTail();
+//        myLinkedList.getLength();  // from prev section of dsa course
 
         System.out.println("\nLinked List:");
         myLinkedList.printList();
