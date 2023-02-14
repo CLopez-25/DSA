038d2589d809eaf9bbc4f6dd789125845a3d6fb4
Cesar Lopez <lopez.m.cesar17@gmail.com>
2023-02-14 16:00:21 -0600

    fixed main method for append exercise

--- src/main/java/datastructures/linkedlist/Main.java
+++ src/main/java/datastructures/linkedlist/Main.java
@@ -8,14 +8,15 @@ public class Main {
 //        this is creating a new linked list. variable mylinkedist is of type linked list which means it can point to a linked list.
 
         LinkedList myLinkedList = new LinkedList(4);  // creating our new linked list with a node with a value of 4 (value was from prev course section).
+//        myLinkedList.getHead();
+//        myLinkedList.getTail();
+//        myLinkedList.getLength();  // from prev section of dsa course
+
 
         myLinkedList.makeEmpty();           // empties the list
         myLinkedList.append(1);             // appends the new node value with a value of 1
         myLinkedList.append(2);             // appends the new node value with a value of 2
 
-//        myLinkedList.getHead();
-//        myLinkedList.getTail();
-//        myLinkedList.getLength();  // from prev section of dsa course
 
         System.out.println("\nLinked List:");
         myLinkedList.printList();
