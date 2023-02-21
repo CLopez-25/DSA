fc8253401487a8ac14bf9e208151e16db92dcdba
Cesar Lopez <lopez.m.cesar17@gmail.com>
2023-02-20 22:44:17 -0600

    completed set section in dll

--- src/main/java/datastructures/doublylinkedlist/Main.java
+++ src/main/java/datastructures/doublylinkedlist/Main.java
@@ -73,7 +73,14 @@ public class Main {
         // end of get exercise ///
 
         // set exercise:
+        DoublyLinkedList myDLL = new DoublyLinkedList(11);
+        myDLL.append(3);
+        myDLL.append(23);
+        myDLL.append(7);
 
+        myDLL.set(1, 4);
+
+        myDLL.printList();
         // end of set exercise ///
 
     }
