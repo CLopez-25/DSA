d77f739dcb5fa7e0d7ce890912f98c83508fbe1f
Cesar Lopez <lopez.m.cesar17@gmail.com>
2023-02-21 00:04:35 -0600

    still adding notes to entire dll section

--- src/main/java/datastructures/doublylinkedlist/Main.java
+++ src/main/java/datastructures/doublylinkedlist/Main.java
@@ -12,8 +12,9 @@ public class Main {
 //        myDLL.getLength();
 //
 //        myDLL.printList();
-
         // end of constructor exercise
+
+
 
         //append exercise:
 //        DoublyLinkedList myDLL = new DoublyLinkedList(1);         // created doubly linked list with the value of 1
@@ -23,19 +24,23 @@ public class Main {
 //        myDLL.printList();                                    // printed doubly linked list
         // end of append exercise ///
 
+
+
         // removeLast exercise:
-//        DoublyLinkedList myDLL = new DoublyLinkedList(1);
-//        myDLL.append(2);
+//        DoublyLinkedList myDLL = new DoublyLinkedList(1);     // created doubly linked list with the value of 1
+//        myDLL.append(2);                                      // appended a node with the value of 2
 //
 //        // (2) Items - Returns 2 Node
 //        System.out.println(myDLL.removeLast().value);
 //
-//        // (1) Item - Return 1 Node
+//        // (1) Item - Return 1 Node                           // returning the values of nodes 1 and 2
 //        System.out.println(myDLL.removeLast().value);
 //
 //        // (0) Items - Return null
-//        System.out.println(myDLL.removeLast());
+//        System.out.println(myDLL.removeLast());        // did not add .value to this because we know it is supposed to return null. .value would give us an error
         // end of removeLast exercise ///
+
+
 
         // prepend exercise:
 //        DoublyLinkedList myDLL = new DoublyLinkedList(2);
@@ -45,6 +50,8 @@ public class Main {
 //
 //        myDLL.printList();
         // end of prepend exercise ///
+
+
 
         // removeFirst exercise:
 //        DoublyLinkedList myDLL = new DoublyLinkedList(2);
@@ -60,6 +67,8 @@ public class Main {
 //        System.out.println(myDLL.removeFirst());
         // end of removeFirst exercise ///
 
+
+
         // get exercise:
 //        DoublyLinkedList myDLL = new DoublyLinkedList(0);
 //        myDLL.append(1);
@@ -72,6 +81,8 @@ public class Main {
 //        myDLL.printList();
         // end of get exercise ///
 
+
+
         // set exercise:
 //        DoublyLinkedList myDLL = new DoublyLinkedList(11);
 //        myDLL.append(3);
@@ -83,6 +94,8 @@ public class Main {
 //        myDLL.printList();
         // end of set exercise ///
 
+
+
         // insert exercise:
 //        DoublyLinkedList myDLL = new DoublyLinkedList(1);
 //        myDLL.append(3);
@@ -91,6 +104,8 @@ public class Main {
 //
 //        myDLL.printList();
         // end of insert exercise ///
+
+
 
         // remove exercise;
         DoublyLinkedList myDLL = new DoublyLinkedList(0);
