c7ee2aeac438d12202a4f515c8cce62344ce679d
Cesar Lopez <lopez.m.cesar17@gmail.com>
2023-02-23 16:32:36 -0600

    completed main method for stack

--- src/main/java/datastructures/stack/Main.java
+++ src/main/java/datastructures/stack/Main.java
@@ -1,8 +1,15 @@
 package datastructures.stack;
 
-public class Main {
+public class Main {                             // main method
 
     public static void main(String[] args) {
+
+        Stack myStack = new Stack(4);     // creating a new stack
+
+        myStack.getTop();
+        myStack.getHeight();                    // getting the top and height values of the stack
+
+        myStack.printStack();                   // prints out the stack using the printStack method
 
     }
 }
