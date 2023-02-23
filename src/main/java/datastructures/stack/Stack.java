b4b1a13eb2b92abcbe2258e847e427c38554ce8f
Cesar Lopez <lopez.m.cesar17@gmail.com>
2023-02-23 16:17:51 -0600

    setting up stack class and constructor

--- src/main/java/datastructures/stack/Stack.java
+++ src/main/java/datastructures/stack/Stack.java
@@ -1,4 +1,22 @@
 package datastructures.stack;
 
 public class Stack {
+
+    private Node top;
+    private int height;
+
+    class Node{
+        int value;
+        Node next;
+
+        Node(int value) {
+            this.value = value;
+        }
+    }
+
+    public Stack(int value){
+        Node newNode = new Node(value);
+        top = newNode;
+        height = 1;
+    }
 }
