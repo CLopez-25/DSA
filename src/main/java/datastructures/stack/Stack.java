513b64bf2ea4149a4c278b9216f2542f94c63254
Cesar Lopez <lopez.m.cesar17@gmail.com>
2023-02-23 16:51:15 -0600

    added notes to push method

--- src/main/java/datastructures/stack/Stack.java
+++ src/main/java/datastructures/stack/Stack.java
@@ -42,14 +42,14 @@ public class Stack {                // stack class
 
     // push method:
     public void push(int value){
-        Node newNode = new Node(value);
-        if (height == 0){
+        Node newNode = new Node(value);     // creating a new node
+        if (height == 0){                   // if statement for when the stack is empty
             top = newNode;
-        } else {
+        } else {                            // else statement for when the stack has items in it
             newNode.next = top;
             top = newNode;
         }
-        height++;
+        height++;                           // incrementing stack by 1
     }
     // end of push method ///
 
