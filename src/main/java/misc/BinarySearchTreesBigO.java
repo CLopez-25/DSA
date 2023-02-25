03fdd684a0380e430def3e0d4ac42a121429fe95
Cesar Lopez <lopez.m.cesar17@gmail.com>
2023-02-24 18:12:17 -0600

    added notes on binary search trees big o

---
+++ src/main/java/misc/BinarySearchTreesBigO.java
@@ -0,0 +1,15 @@
+package misc;
+
+public class BinarySearchTreesBigO {
+
+    // binary search trees are more efficient when using lookup() and remove() because of O(log n)
+    // linked lists are not because youd have to iterate through all indexes or reindex items in the list
+    // comparisons apply to array lists as well
+    // linked lists and binary search trees are both dynamic sized data structures
+
+
+    // in an unsorted array list if you add an item to the end its just O(1) so its better than a binary search tree
+    // but if you were looking up or removing by value, not by index, it would be O(n)
+
+
+}
