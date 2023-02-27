5034448207aa864cf486c1b0d62c4f0fe9be2725
Cesar Lopez <lopez.m.cesar17@gmail.com>
2023-02-27 14:24:30 -0600

    completed bst constructor section

--- src/main/java/datastructures/binarysearchtree/Main.java
+++ src/main/java/datastructures/binarysearchtree/Main.java
@@ -4,5 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
 
+        BinarySearchTree myBST = new BinarySearchTree();
+
+        System.out.println("root = " + myBST);
     }
 }
