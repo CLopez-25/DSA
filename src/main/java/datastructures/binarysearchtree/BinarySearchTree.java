6e85d43760aa6d820213e75da2f9a74c57821849
Cesar Lopez <lopez.m.cesar17@gmail.com>
2023-02-27 14:17:32 -0600

    created bst class

--- src/main/java/datastructures/binarysearchtree/BinarySearchTree.java
+++ src/main/java/datastructures/binarysearchtree/BinarySearchTree.java
@@ -2,4 +2,16 @@ package datastructures.binarysearchtree;
 
 public class BinarySearchTree {
 
+    Node root;      // not private because we wanna have access so we have visibility to it
+
+    class Node{
+        int value;
+        Node left;
+        Node right;
+
+        private Node(int value){
+            this.value = value;
+        }
+    }
+
 }
