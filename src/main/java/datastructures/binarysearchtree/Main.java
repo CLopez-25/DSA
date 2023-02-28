39aea36b91f9d3c74b3d7681babf4311f180338e
Cesar Lopez <lopez.m.cesar17@gmail.com>
2023-02-28 14:36:11 -0600

    completed insert section in bst

--- src/main/java/datastructures/binarysearchtree/Main.java
+++ src/main/java/datastructures/binarysearchtree/Main.java
@@ -4,8 +4,24 @@ public class Main {
 
     public static void main(String[] args) {
 
+//        BinarySearchTree myBST = new BinarySearchTree();
+//
+//        System.out.println("root = " + myBST);
+        // end of first section
+
+        // insert section:
         BinarySearchTree myBST = new BinarySearchTree();
 
-        System.out.println("root = " + myBST);
+        myBST.insert(47);
+        myBST.insert(21);
+        myBST.insert(76);
+        myBST.insert(18);
+        myBST.insert(52);
+        myBST.insert(82);
+
+        myBST.insert(27);
+
+        System.out.println(myBST.root.left.right.value);
+        // end of insert section ///
     }
 }
