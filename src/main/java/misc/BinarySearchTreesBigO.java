b255a8d8a2ebc19c4e31c5ce0d2e66752b9c8b7c
Cesar Lopez <lopez.m.cesar17@gmail.com>
2023-02-28 14:01:01 -0600

    adding notes for insert in bst section

--- src/main/java/misc/BinarySearchTreesBigO.java
+++ src/main/java/misc/BinarySearchTreesBigO.java
@@ -17,6 +17,17 @@ public class BinarySearchTreesBigO {
     // create newNode
     // if less than go left,  else greater than go right
     // if null insert newNode else move to next
+    // if there is an indication of a loop, figure out if it's a while loop or a for loop
+    // if you know how many steps you're going to iterate through, it's a for loop.
+    // if you don't then it's a while loop
+    // pointer variable will be called temp and it'll be set to be equal to root
+
+    // in an empty tree scenario you'll set the root to be equal to the new node, i.e.:
+    // create newNode
+    // if root == null then root == newNode
+
+    // you cant insert a value that is already in the binary search tree
+
     //
 
 }
