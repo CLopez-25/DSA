4407ebacf067014c09d8fbc2bd4792d9bd66314b
Cesar Lopez <lopez.m.cesar17@gmail.com>
2023-03-02 12:31:53 -0600

    completed hashtable method

--- src/main/java/datastructures/hashtable/HashTable.java
+++ src/main/java/datastructures/hashtable/HashTable.java
@@ -15,5 +15,18 @@ public class HashTable {
         }
     }
 
+    public HashTable(){
+        dataMap = new Node[size];
+    }
+
+    public void Printtable(){
+        for (int i = 0; i < dataMap.length; i++){
+            System.out.println(i + ":");
+            Node temp = dataMap[i];
+            while (temp != null) {
+                System.out.println("   {" + temp.key + "= " + temp.value + "}");
+            }
+        }
+    }
 
 }
