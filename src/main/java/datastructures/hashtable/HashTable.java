dc16430a1fabaa88868d40f7a4bdde346e1d4241
Cesar Lopez <lopez.m.cesar17@gmail.com>
2023-03-02 12:25:14 -0600

    completing hash table class methods

--- src/main/java/datastructures/hashtable/HashTable.java
+++ src/main/java/datastructures/hashtable/HashTable.java
@@ -1,4 +1,19 @@
 package datastructures.hashtable;
 
 public class HashTable {
+    private int size = 7;
+    private Node[] dataMap;
+
+    class Node{
+        String key;
+        int value;
+        Node next;
+
+        Node(String key, int value){
+            this.key = key;
+            this.value = value;
+        }
+    }
+
+
 }
