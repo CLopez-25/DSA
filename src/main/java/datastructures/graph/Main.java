0bb07961ef6420f8d1f632aaad13846995f7bc3b
Cesar Lopez <lopez.m.cesar17@gmail.com>
2023-03-04 02:01:49 -0600

    completed remove edge section

--- src/main/java/datastructures/graph/Main.java
+++ src/main/java/datastructures/graph/Main.java
@@ -24,6 +24,17 @@ public class Main {
         // remove edge:
         Graph myGraph = new Graph();
 
+        myGraph.addVertex("A");
+        myGraph.addVertex("B");
+        myGraph.addVertex("C");
+
+        myGraph.addEdge("A", "B");
+        myGraph.addEdge("A", "C");
+        myGraph.addEdge("B", "C");
+
+        myGraph.removeEdge("A", "B");
+
+        myGraph.printGraph();
         // end of remove edge
 
     }
