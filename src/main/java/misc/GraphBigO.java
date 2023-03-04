eb9b353bd13d5f7e140bbffb148292395cca12c2
Cesar Lopez <lopez.m.cesar17@gmail.com>
2023-03-04 01:03:29 -0600

    completed notes on hash tables and started graphs section

---
+++ src/main/java/misc/GraphBigO.java
@@ -0,0 +1,12 @@
+package misc;
+
+public class GraphBigO {
+
+    // for space complexity, the adjacency matrix is O of the number of vertices squared. O(|V|^2)
+    // adjacency list is O of the number of vertices plus the number of edges. O(|V|+|E|)
+    // so from a space complexity, the adjacency list is better.
+
+    // adding a vertex is just a matter of adding to the end of the list in an adjacency list,
+    // whereas in an adjacency matrix you have to add a row and a column. it is stored in a two-dimensional array,
+    // as soon as you make those arrays bigger, you have to create new arrays that are bigger
+}
