3c6dd3ca7d95ada519dbdf0b44223f7dec9fd249
Cesar Lopez <lopez.m.cesar17@gmail.com>
2023-03-04 01:08:50 -0600

    fixed notes on graph big o

--- src/main/java/misc/GraphBigO.java
+++ src/main/java/misc/GraphBigO.java
@@ -2,11 +2,15 @@ package misc;
 
 public class GraphBigO {
 
-    // for space complexity, the adjacency matrix is O of the number of vertices squared. O(|V|^2)
-    // adjacency list is O of the number of vertices plus the number of edges. O(|V|+|E|)
+    // for space complexity, the ADJACENCY MATRIX is O of the number of vertices squared. O(|V|^2)
+    // ADJACENCY LIST is O of the number of vertices plus the number of edges. O(|V|+|E|)
     // so from a space complexity, the adjacency list is better.
 
-    // adding a vertex is just a matter of adding to the end of the list in an adjacency list,
+    // ADDING a vertex is just a matter of adding to the end of the list in an adjacency list,
     // whereas in an adjacency matrix you have to add a row and a column. it is stored in a two-dimensional array,
-    // as soon as you make those arrays bigger, you have to create new arrays that are bigger
+    // as soon as you make those arrays bigger, you have to create new arrays that are bigger, which is inefficient.
+
+    // FROM A BIG O perspective, ADJACENCY MATRIX is O(|V|^2) every time you add a vertex,
+    // whereas with an ADJACENCY LIST it's O(1) to ADD a vertex!
+
 }
