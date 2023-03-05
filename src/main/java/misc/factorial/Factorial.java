d4cdc1686bde60b26f33ea0f90d4706e32ebe421
Cesar Lopez <lopez.m.cesar17@gmail.com>
2023-03-05 17:49:27 -0600

    completed factorial section

--- src/main/java/misc/factorial/Factorial.java
+++ src/main/java/misc/factorial/Factorial.java
@@ -6,4 +6,11 @@ public class Factorial {
         if (n == 1) return 1;
         return n * factorial(n - 1);
     }
+
+    public static void main(String[] args) {
+
+        int myFac = factorial(4);
+
+        System.out.println(myFac);
+    }
 }
