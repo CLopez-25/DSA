cc4ca0b23acf1f3aeb27b33996c05c29b821c1e3
Cesar Lopez <lopez.m.cesar17@gmail.com>
2023-03-07 13:02:47 -0600

    completed minvalue section

--- src/main/java/datastructures/binarysearchtree/Main.java
+++ src/main/java/datastructures/binarysearchtree/Main.java
@@ -59,22 +59,44 @@ public class Main {
         // end of rcontains section ///
 
         // rinsert section:
+//        BinarySearchTree myBST = new BinarySearchTree();
+//
+//        myBST.insert(2);
+//        myBST.insert(1);
+//        myBST.insert(3);
+//
+//        /* lines above, create this tree:
+//                  2
+//                /   \
+//               1     3
+//        */
+//
+//        System.out.println("\nRoot: " + myBST.root.value);
+//        System.out.println("\nRoot->Left: " + myBST.root.left.value);
+//        System.out.println("\nRoot->Right: " + myBST.root.right.value);
+        // end of rinsert section ///
+
         BinarySearchTree myBST = new BinarySearchTree();
 
-        myBST.insert(2);
-        myBST.insert(1);
-        myBST.insert(3);
+        myBST.insert(47);
+        myBST.insert(21);
+        myBST.insert(76);
+        myBST.insert(18);
+        myBST.insert(27);
+        myBST.insert(52);
+        myBST.insert(82);
 
-        /* lines above, create this tree:
-                  2
-                /   \
-               1     3
-        */
+        System.out.println(myBST.minValue(myBST.root));
 
-        System.out.println("\nRoot: " + myBST.root.value);
-        System.out.println("\nRoot->Left: " + myBST.root.left.value);
-        System.out.println("\nRoot->Right: " + myBST.root.right.value);
-        // end of rinsert section ///
+        System.out.println(myBST.minValue(myBST.root.right));
+
+            /* lines above, create this binary search tree:
+                      47
+                    /   \
+                   21    76
+                 /  \   /  \
+                18  27 52   82
+            */
 
     }
 }
