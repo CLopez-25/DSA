9fd373fd6505bd6c9f4ee7c9acdbb5867b830963
Cesar Lopez <lopez.m.cesar17@gmail.com>
2023-03-08 23:59:26 -0600

    strted method

--- src/main/java/algorithms/sorts/insertionsort/InseertionSort.java
+++ src/main/java/algorithms/sorts/insertionsort/InseertionSort.java
@@ -2,6 +2,10 @@ package algorithms.sorts.insertionsort;
 
 public class InseertionSort {
 
+    public static void insertionSort(int[] array){
+        for (int i = 1; i < array.length; i++){
 
+        }
+    }
 
 }
