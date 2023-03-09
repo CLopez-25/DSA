06a8bb8791d0f436ab9d38895dcb1d4bb19b3cef
Cesar Lopez <lopez.m.cesar17@gmail.com>
2023-03-09 00:01:41 -0600

    completed insertion method

--- src/main/java/algorithms/sorts/insertionsort/InseertionSort.java
+++ src/main/java/algorithms/sorts/insertionsort/InseertionSort.java
@@ -4,7 +4,13 @@ public class InseertionSort {
 
     public static void insertionSort(int[] array){
         for (int i = 1; i < array.length; i++){
-
+            int temp = array[i];
+            int j = i - 1;
+            while (j > -1 && temp < array[j]) {
+                array[j+1] = array[j];
+                array[j] = temp;
+                j--;
+            }
         }
     }
 
