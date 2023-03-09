96e15cd3b84ad3601a7b0cebbe4c6dc3c46d7b43
Cesar Lopez <lopez.m.cesar17@gmail.com>
2023-03-09 13:46:53 -0600

    added notes for merge sort

---
+++ src/main/java/algorithms/sorts/mergesort/MergeSort.java
@@ -0,0 +1,7 @@
+package algorithms.sorts.mergesort;
+
+public class MergeSort {
+
+
+
+}
---
+++ src/main/java/misc/misc/MergeSortNotes.java
@@ -0,0 +1,11 @@
+package misc.misc;
+
+public class MergeSortNotes {
+
+    /*
+        Merge sort is as efficient as you can make a sorting algorithm because it breaks everything down into single item arrays.
+
+
+     */
+
+}
