c7fe9535529f06820777bb4f74768a0fb8b2cc91
Cesar Lopez <lopez.m.cesar17@gmail.com>
2023-03-09 00:33:46 -0600

    completed notes on basic sorts big o

--- src/main/java/misc/BasicSortsBigO.java
+++ src/main/java/misc/BasicSortsBigO.java
@@ -1,6 +1,6 @@
 package misc;
 
-public class InsertionSortBigO {
+public class BasicSortsBigO {
 
     /*
 
@@ -17,6 +17,16 @@ public class InsertionSortBigO {
 
      except when you have almost sorted data, you can use an insertion algorithm that is more
      primitive because it is O(n^2) typically and it will actually run faster.
+
+  Quiz Notes:
+
+  Bubble, Selection, and Insertion Sort all have loops within loops,
+  so they are O(n^2) in time complexity.
+
+  Bubble, Selection, and Insertion Sort have O(1) space complexity. all sort the array in place
+  so that means they don't create additional copies of the array.
+
+  Only Insertion Sort is O(n) if you start with a sorted (or almost sorted) array!!
 
      */
 
