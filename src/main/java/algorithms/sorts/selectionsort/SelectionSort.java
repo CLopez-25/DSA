5af83c39d0da264e9b7d64a2fdf904faffde7f86
Cesar Lopez <lopez.m.cesar17@gmail.com>
2023-03-08 23:53:44 -0600

    working on selection sort section

--- src/main/java/algorithms/sorts/selectionsort/SelectionSort.java
+++ src/main/java/algorithms/sorts/selectionsort/SelectionSort.java
@@ -1,4 +1,24 @@
 package algorithms.sorts.selectionsort;
 
+import java.util.Arrays;
+
 public class SelectionSort {
+
+    public static void selectionSort(int[] array){
+        for (int i = 0; i < array.length; i++) {
+            int minIndex = i;
+            for (int j = i+1; j < array.length; j++) {
+                if (array[j] < array[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            if (i != minIndex) {
+                int temp = array[i];
+                array[i] = array[minIndex];
+                array[minIndex] = temp;
+            }
+        }
+    }
+
+
 }
