806a8f8e994b31d0e7932f9015efe57fadd9109c
Cesar Lopez <lopez.m.cesar17@gmail.com>
2023-03-11 15:41:16 -0600

    added main method for merge sort

--- src/main/java/algorithms/sorts/mergesort/MergeSort.java
+++ src/main/java/algorithms/sorts/mergesort/MergeSort.java
@@ -33,12 +33,12 @@ public class MergeSort {
         return combined;
     }
 
-    public static void main(String[] args) {
-        int[] array1 = {1,3,7,8};
-        int[] array2 = {2,4,5,6};
-
-        System.out.println(Arrays.toString(merge(array1, array2)));
-    }
+//    public static void main(String[] args) {
+//        int[] array1 = {1,3,7,8};
+//        int[] array2 = {2,4,5,6};
+//
+//        System.out.println(Arrays.toString(merge(array1, array2)));
+//    }
 
     public static int[] mergeSort(int[] array) {
         if (array.length == 1) return array;
@@ -48,6 +48,15 @@ public class MergeSort {
         int[] right = mergeSort(Arrays.copyOfRange(array, midindex, array.length));
 
         return merge(left,right);
+    }
+
+    public static void main(String[] args) {
+        int[] originalArray = {3,1,4,2};
+        int[] sortedArray = mergeSort(originalArray);
+
+        System.out.println("\nOriginal Array: " + Arrays.toString(originalArray));
+
+        System.out.println("\nSorted Array: " + Arrays.toString(sortedArray));
     }
 
 }
