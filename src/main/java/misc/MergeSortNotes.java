8bc3f6c6bb8d274f5953374631e39cc413a25a49
Cesar Lopez <lopez.m.cesar17@gmail.com>
2023-03-11 16:26:33 -0600

    added notes for merge sort big o

---
+++ src/main/java/misc/MergeSortNotes.java
@@ -0,0 +1,22 @@
+package misc;
+
+public class MergeSortNotes {
+
+/*
+
+    Merge sort is as efficient as you can make a sorting algorithm because
+    it breaks everything down into single item arrays.
+
+    the space complexity is O(n). in the example you have 8 items in the array and then
+    you had to create 8 new items.
+
+    breaking the example apart and iterating thru the array is O(log n)-
+    and putting it back together would be O(n), which gives us O(n log n).
+    when it comes to sorting algorithms O(n^2) and O(n log n) are pretty much your two options.
+        (there are some exceptions to this with sorting only numbers because
+        there are some efficiencies when sorting only numbers.)
+
+
+ */
+
+}
