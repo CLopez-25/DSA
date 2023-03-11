d25a4f5aa5da0fcd9125dfbc043f2861537dd1af
Cesar Lopez <lopez.m.cesar17@gmail.com>
2023-03-11 17:44:52 -0600

    added notes to quick sort big o

---
+++ src/main/java/misc/QuickSortBigO.java
@@ -0,0 +1,19 @@
+package misc;
+
+public class QuickSortBigO {
+
+/*
+    from a space complexity perspective when we run a pivot this is O(1).
+    this is an advantage of quick sort over mere sort.
+
+    from a time complexity it is O(n) because we had a
+    for loop that went from the beginning to the end,
+    and if you look at how many times we had to do it, it was three. that's O(log n)
+    so what you end up with is O(n log n), which is better than O(n^2). but that is actually the best case.
+    the worst case is if you have already sorted data!
+        remember big o looks at worst case
+
+
+*/
+
+}
