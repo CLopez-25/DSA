bdc8cc84ae8e8e240c5d09d0d67de9b41eabaad2
Cesar Lopez <lopez.m.cesar17@gmail.com>
2023-03-11 14:26:36 -0600

    completed merge sort method

--- src/main/java/algorithms/sorts/mergesort/MergeSort.java
+++ src/main/java/algorithms/sorts/mergesort/MergeSort.java
@@ -9,13 +9,26 @@ public class MergeSort {
         int j = 0;
         while (i < array1.length && j < array2.length) {
             if (array1[i] < array2[j]) {
-                combined[index] = array1;
+                combined[index] = array1[i];
                 index++;
                 i++;
             } else {
                 combined[index] = array2[j];
+                index++;
+                j++;
             }
         }
+        while (i < array1.length) {
+            combined[index] = array1[i];
+            index++;
+            i++;
+        }
+        while (j < array2.length) {
+            combined[index] = array2[j];
+            index++;
+            j++;
+        }
+        return combined;
     }
 
 }
