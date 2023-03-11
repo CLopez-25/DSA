db5c0f6186280a51f47ce7d117b99f764d6e8900
Cesar Lopez <lopez.m.cesar17@gmail.com>
2023-03-11 16:37:49 -0600

    working on quick sort method

---
+++ src/main/java/algorithms/sorts/quicksort/QuickSort.java
@@ -0,0 +1,13 @@
+package algorithms.sorts.quicksort;
+
+public class QuickSort {
+
+    private static void swap(int[] array, int firstIndex, int secondIndex) {
+        int temp = array[firstIndex];
+        array[firstIndex] = array[secondIndex];
+        array[secondIndex] = temp;
+    }
+
+
+
+}
--- src/main/java/misc/MergeSortBigO.java
+++ src/main/java/misc/MergeSortBigO.java
@@ -1,6 +1,6 @@
 package misc;
 
-public class MergeSortNotes {
+public class MergeSortBigO {
 
 /*
 
