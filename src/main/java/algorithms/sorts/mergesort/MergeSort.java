03a9133e49e9fb9e78258ec1404584bcb2d8114c
Cesar Lopez <lopez.m.cesar17@gmail.com>
2023-03-11 14:23:39 -0600

    started merge sort method

--- src/main/java/algorithms/sorts/mergesort/MergeSort.java
+++ src/main/java/algorithms/sorts/mergesort/MergeSort.java
@@ -2,6 +2,20 @@ package algorithms.sorts.mergesort;
 
 public class MergeSort {
 
-
+    public static int[] merge(int[] array1, int[] array2) {
+        int[] combined = new int[array1.length + array2.length];
+        int index = 0;
+        int i = 0;
+        int j = 0;
+        while (i < array1.length && j < array2.length) {
+            if (array1[i] < array2[j]) {
+                combined[index] = array1;
+                index++;
+                i++;
+            } else {
+                combined[index] = array2[j];
+            }
+        }
+    }
 
 }
